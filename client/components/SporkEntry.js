25555e3e80e569626a17764db2441294272b839f
Silverhawk <sondra.silverhawk@hush.com>
2016-03-01 19:41:52 -0800

    Create spork entry component

---
+++ client/components/SporkEntry.js
@@ -0,0 +1,9 @@
+const React = require('react');
+
+const SporkEntry = (props) => {
+  <div className="spork">
+    <span> <img src="spork"+{key}/> {props.number} </span>
+  </div>
+}
+
+module.exports = SporkEntry;
