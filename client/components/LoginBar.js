9ddf20f107bf20462b12960fafca9ab90f4fa1b3
shivg <docshivaguru@gmail.com>
2016-03-01 19:54:06 -0800

    Adds loginBar component and conditionally loads it on checking isLoggedIn in state

---
+++ client/components/LoginBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+const TicketList = require('./TicketList');
+
+
+const LoginBar = (props) => (
+	<div>
+		<ul className = 'login-bar'>
+			<li>Contribute to Open Source Projects.</li>
+			<li>Earn Sporks by finding and solving outstanding issues in your favorite open source projects</li>
+			<button><a href="http://localhost:3000/auth/github">Login with Github</a></button>
+		</ul>
+		<TicketList />
+	</div>
+	)
+
+module.exports = LoginBar;
--- client/components/app.js
+++ client/components/app.js
@@ -1,6 +1,7 @@
 const React = require('react');
 const NavBar = require('./NavBar'); 
- 
+const LoginBar = require('./LoginBar');
+
 const linksList = [
   {
     name: "Tickets", url: '/'
@@ -19,12 +20,14 @@ const App = class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      route: '/'
+      route: '/',
+      isLoggedIn: false
     };
   }
   
   render () {
     return (
+    !this.state.isLoggedIn ? <LoginBar /> :
     <div className='app-shell grey lighten-2'>
       <NavBar links={linksList}/>
       <div className="row">
