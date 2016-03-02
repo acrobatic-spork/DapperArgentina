c12e2ee9610fa254f86c034d342af78e7851f865
Gerrit <gerrityntema@gmail.com>
2016-03-01 20:02:49 -0800

    Add user info utility function

--- client/js/users.js
+++ client/js/users.js
@@ -18,3 +18,20 @@ module.exports.getSporks = function(successCallback, errCallback, user) {
     return successCallback(data);
   }, errCallback, user);
 };
+
+var getUserInfoFromApi = function (successCallback, errCallback, user) {
+  var options = {
+    url: 'http://45.55.29.22:3000/api/user', // add route to query users db for github info
+    type: 'GET',
+    success: successCallback,
+    error: errCallback
+  };
+
+  $.ajax(options);  
+};
+
+module.exports.getUserInfo = function(successCallback, errCallback, user) {
+  getUserFromApi((data) => {
+    return successCallback(data);
+  }, errCallback, user);
+};
