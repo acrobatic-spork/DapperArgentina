49bb00a8e7aac2578576c994a34a1dfe5818fe9e
Nick Echols <stere.ereo@gmail.com>
2016-03-02 17:38:31 -0800

    More styling

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -17,8 +17,8 @@ const NavBar = class NavBar extends React.Component {
     return (
       <div className="navbar-fixed">
         <nav className="cyan lighten-2" role="navigation">
-          <div className="nav-wrapper container col s12 m4 l8"><a id="logo-container" href="#" className="brand-logo">SporkHub</a>
-            <ul className="right hide-on-med-and-down">
+          <div className="nav-wrapper container col s12 l8"><a id="logo-container" href="#" className="">SporkHub</a>
+            <ul className="right hide-on-small-and-down">
               {this.props.links.map ((link, index) => 
                 <NavEntry data={link} key={index} />
               )}
@@ -28,7 +28,7 @@ const NavBar = class NavBar extends React.Component {
                 <NavEntry data={link} key={index} />
               )}
             </ul>
-            <a href="#" data-activates="nav-mobile" className="button-collapse"><i className="material-icons">menu</i></a>
+            <a href="#" data-activates="nav-mobile" className="button-collapse hide-on-med-and-up"><i className="material-icons">menu</i></a>
           </div>
         </nav>
       </div>
