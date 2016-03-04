c25d1bf567ab173283e0325c0387843c9d8cef71
Silverhawk <sondra.silverhawk@hush.com>
2016-03-03 20:31:33 -0800

    Take modal out of resources

--- client/components/ResourceList.js
+++ client/components/ResourceList.js
@@ -12,9 +12,6 @@ const ResourceList = (props) => (
       Want to contribute to open source projects on GitHub, but unsure where to start? You've come to the right place!
       We comb through active repos to find the easy issues that beginners can handle. Still not ready to dive in? Check out these guides:
       </blockquote>
-
-      <ConfirmFork />
-
       <ul className="collapsible popout" data-collapsible="accordion">
         <li>
           <div className="collapsible-header active"><i className="material-icons">filter_drama</i><strong>How to Play</strong></div>
