6e6d4acb6b99ef4efd763c64259121ed94a237c1
Gerrit <gerrityntema@gmail.com>
2016-03-03 21:43:54 -0800

    Refactor users.js ajax calls

--- client/js/users.js
+++ client/js/users.js
@@ -2,38 +2,31 @@ const $ = require('jquery');
 
 module.exports = {};
 
-var getUserInfoFromApi = function (successCallback, errCallback, user) {
+module.exports.getUserInfo = function(successCallback, errCallback, user) {
   var options = {
     url: '/api/users/'+user, // add route to query users db for github info
     type: 'GET',
-    success: successCallback,
-    error: errCallback
+    success: function(data) {
+      successCallback(data)
+    },
+    error: function(error) {
+      errCallback(error)
+    }
   };
 
-  $.ajax(options);  
+  $.ajax(options); 
 };
 
-// Get a url for pic, array of sporks...
-module.exports.getUserInfo = function(successCallback, errCallback, user) {
-  getUserInfoFromApi((data) => {
-    return successCallback(data);
-  }, errCallback, user);
-};
-
-var getUsersFromApi = function (successCallback, errCallback) {
+module.exports.getUsers = function(successCallback, errCallback) {
   var options = {
     url: '/api/users',
     type: 'GET',
-    success: successCallback,
-    error: errCallback
+    success: function (data) {
+      successCallback(data);
+    },
+    error: function (error) {
+      errCallback(error);
+    }
   };
-
-  $.ajax(options);  
-};
-
-// Get a url for pic, array of sporks...
-module.exports.getUsers = function(successCallback, errCallback) {
-  getUsersFromApi((data) => {
-    return successCallback(data);
-  }, errCallback);
+  $.ajax(options);
 };
