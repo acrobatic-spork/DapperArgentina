8479d54354d5ff842c4ab9031071058ac7b76d0d
shivg <docshivaguru@gmail.com>
2016-03-04 11:05:32 -0800

    Adds UserNavEntry

--- client/components/UserNavSub.js
+++ client/components/UserNavSub.js
@@ -7,7 +7,7 @@ const UserNav = function (props) {
               <div className="nav-wrapper container col s12 l8">
                 <ul className="right hide-on-small-and-down">
                   {this.props.links.map ((link, index) => 
-                    <UserNavEntrt data={link} key={index} />
+                    <UserNavEntry data={link} key={index} />
                   )}
                   <li>
                     <a href='' onClick={Auth.logOut}>Log Out</a>
@@ -15,7 +15,7 @@ const UserNav = function (props) {
                 </ul>
                 <ul id="nav-mobile" className="side-nav">
                   {this.props.links.map ((link, index) => 
-                    <UserNavEntrt data={link} key={index} />
+                    <UserNavEntry data={link} key={index} />
                   )}
                 </ul>
                 <a href="#" data-activates="nav-mobile" className="button-collapse hide-on-med-and-up"><i className="material-icons">menu</i></a>
