272c625c089096a520e2906b283b5c512fb44abd
Nick Echols <stere.ereo@gmail.com>
2016-03-04 15:19:01 -0800

    Improve fork button modal workflow

--- client/components/ConfirmFork.js
+++ client/components/ConfirmFork.js
@@ -1,40 +1,69 @@
 import React, {PropTypes} from 'react';
 import {ModalContainer, ModalDialog} from 'react-modal-dialog';
+const forkUtil = require('../js/fork');
+
+
+const ForkInstructions = (props) => (
+  <div>
+    <p>Clone (copy) the repo by opening the terminal and running</p>
+    <pre><code>git clone https://github.com/{props.forkInfo.username}/{props.data.name}.git</code></pre>
+    <p>The link depends on the repo, and can be found on GitHub.</p>
+    <p>Add the original repo as an upstream remote</p>
+    <p>In the repo's main directory, run:</p>
+    <pre><code>git remote add upstream https://github.com/{props.forkInfo.org_name}/{props.data.name}.git</code></pre>
+    <p><strong>Make changes and commit!</strong></p>
+
+    <a className="cyan-text lighten-2 cyan-text lighten-2" href="/resources">Visit our Getting Started page for more details</a>
+  </div>
+);
+
+
+
 
 class ConfirmFork extends React.Component {
 
   constructor (props) {
     super(props);
+    this.state = {
+      isForked: false,
+      forkInfo: {}
+    }
+    this.style = { borderRadius: 0 }
   }
+
+  forkRepo () {
+    forkUtil.forkRepo(function (data) {
+      console.log('successfully forked repo: ' + JSON.stringify(data));
+      this.setState({isForked: true, forkInfo: data});
+    }.bind(this), console.error, this.props.data.org_name, this.props.data.name, this.props.username);
+  }
+
+
 
   handleClick () { this.props.openModel() }
   handleClose () { this.props.closeModal() }
   handleFork(e) {
     e.preventDefault();
-    this.props.fork();
+    this.forkRepo();
   }
   render() {
     return (<div onClick={this.handleClick}>
       {
         this.props.isShowing &&
         <ModalContainer onClose={this.handleClose.bind(this)}>
-          <ModalDialog onClose={this.handleClose.bind(this)}>
+          <ModalDialog style={this.style} onClose={this.handleClose.bind(this)}>
             <h4>You're about to fork a repo!</h4>
-            <p>Clone (copy) the repo by opening the terminal and running</p> <pre>git clone "link to your forked repo"</pre>
-            <p>The link depends on the repo, and can be found on GitHub.</p>
-            <p>Add the original repo as an upstream remote</p>
-            <p>In the repo's main directory, run:</p> <pre>git remote add upstream "link to the original repo"</pre>
-            <p><strong>Make changes and commit!</strong></p>
-
-            <button onClick={this.handleFork.bind(this)}>Fork it!</button>
-
-            <a className="cyan-text lighten-2 cyan-text lighten-2" href="/resources">Visit our Getting Started page for more details</a>
+            <div>this will make a fork on your GitHub account for you to start hacking on</div>
+            <a className={"btn cyan" + (this.state.isForked ? " disabled" : "")} onClick={this.handleFork.bind(this)}><i className="octicon octicon-git-forked"></i>{this.state.isForked ? "Forked!" : "Fork It!"}</a>
+            {this.state.isForked && <ForkInstructions data={this.props.data} forkInfo={this.state.forkInfo} />}
           </ModalDialog>
         </ModalContainer>
       }
     </div>);
   }      
 }
+
+
 
 
 module.exports = ConfirmFork;
