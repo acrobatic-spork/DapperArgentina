3be4f709765e27fd758f410991e0c4158b0a2565
shivg <docshivaguru@gmail.com>
2016-03-04 14:13:35 -0800

    Show friends has a navbar as well

--- client/components/ShowFriends.js
+++ client/components/ShowFriends.js
@@ -1,6 +1,15 @@
 const React = require('react');
 const getFriends = require('../js/friends').getFriends;
+const UserNav = require('./UserNav');
 
+const navLinks = [
+  {
+    name: "Users / Add Users", url: '/users'
+  },
+  {
+    name: "Friends", url: '/friends'
+  }
+];
 
 const ShowFriends = class ShowFriends extends React.Component {
   constructor(props){
@@ -27,16 +36,21 @@ const ShowFriends = class ShowFriends extends React.Component {
   }
 
   render() {
-    if(this.state.friendsToRender.length === 0) return (<h1>No friends yet</h1>)
+    if(this.state.friendsToRender.length === 0) return (
+      <div>
+        <UserNav links={navLinks}/>
+        <h2>No friends yet. Add some friends <a href='/#/users'>here</a></h2>
+      </div>)
     else {
       var FriendEntry = () => (
-        <div>
-          <img src={props.friend.avatar_url} width="50" />
+        <div className='card-content'>
+          <img src={props.friend.avatar_url} />
           {props.friend.name} <br/> {props.friend.html_url} <br/>
         </div>)
 
       return (
       <div>
+      <UserNav links={navLinks}/>
       <div class='all-friends-view'>
       {this.state.friendsToRender.map((friend, index) => {
         return (<FriendEntry friend={friend} key={index}/>)
--- client/components/UserEntry.js
+++ client/components/UserEntry.js
@@ -4,11 +4,13 @@ const UserEntry = function (props){
   console.log('In UserEntry')
   return (
     <div className='card-panel hoverable'>
-      <img className='card-image' src={props.user.avatar_url} width="50" />
-      <span><h5>{props.user.name}</h5> </span> 
-      <p>
-      Github: <a href={props.user.html_url}>See @{props.user.username} at Github</a>
-      </p>
+      <div className='card-content'>
+        <img className='card-image' src={props.user.avatar_url} width='100px'  />
+        <span><h5>{props.user.name}</h5> </span> 
+        <p>
+        Github: <a href={props.user.html_url}>See @{props.user.username} at Github</a>
+        </p>
+      </div>
     </div>
     )
 }
