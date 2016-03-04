e0c6f3672a2bec567eee0776bd6c5f2bb786c719
Gerrit <gerrityntema@gmail.com>
2016-03-03 21:49:21 -0800

    Refactor friends.js ajax calls

--- client/js/friends.js
+++ client/js/friends.js
@@ -2,25 +2,22 @@ const $ = require('jquery');
 
 module.exports = {};
 
-var getFriendsFromApi = function (successCallback, errCallback, user_id) {
+var getFriends = function (successCallback, errCallback, user_id) {
   var options = {
     url: '/api/friend?user_id='+user_id, // add route to query users db for github info
     type: 'GET',
-    success: successCallback,
-    error: errCallback
+    success: function (data) {
+      successCallback(data)
+    },
+    error: function(error) {
+      errCallback(error)
+    }
   };
 
   $.ajax(options);  
 };
 
-// Get a url for pic, array of sporks...
-module.exports.getFriends = function(successCallback, errCallback, user_id) {
-  getFriendsFromApi((data) => {
-    return successCallback(data);
-  }, errCallback, user_id);
-};
-
-var addFriendToApi = function (successCallback, errCallback, user_id, friend_id) {
+var addFriend = function (successCallback, errCallback, user_id, friend_id) {
   var options = {
     url: '/api/friend', // add route to query users db for github info
     type: 'POST',
@@ -28,16 +25,13 @@ var addFriendToApi = function (successCallback, errCallback, user_id, friend_id)
       user_id: user_id,
       friend_id: friend_id
     }
-    success: successCallback,
-    error: errCallback
+    success: function (data) {
+      successCallback(data)
+    },
+    error: function(error) {
+      errCallback(error)
+    }
   };
 
   $.ajax(options);  
-};
-
-// Get a url for pic, array of sporks...
-module.exports.addFriend = function(successCallback, errCallback, user_id, friend_id) {
-  addFriendToApi((data) => {
-    return successCallback(data);
-  }, errCallback, user_id);
 };
