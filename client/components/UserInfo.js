596ce83511b78216600b610dc2bd8362343aad53
Silverhawk <sondra.silverhawk@hush.com>
2016-03-04 18:06:39 -0800

    Make fork disappear after it is deleted

--- client/components/UserInfo.js
+++ client/components/UserInfo.js
@@ -49,7 +49,7 @@ class UserInfo extends React.Component {
           </div>
         </div>
         <div className="col s12">        
-         <UserRepoList repos={this.props.userRepos} username={this.props.username} />
+         <UserRepoList repos={this.props.userRepos} username={this.props.username} refreshUserInfo={this.props.refreshUserInfo}/>
         </div>
       </div>
     );
