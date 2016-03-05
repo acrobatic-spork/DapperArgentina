29e622277a77ec481a59e0f60778c9f905fdebbf
Nick Echols <stere.ereo@gmail.com>
2016-03-05 14:19:17 -0800

    Fix loading animation centering

--- client/components/LoadingAnimation.js
+++ client/components/LoadingAnimation.js
@@ -1,11 +1,11 @@
 const React = require('react');
 
 const LoadingAnimation = function (props){
-  console.log('In LoadingAnimation')
   return (
     <div className="anim-box" >
-    <span className="cyan-text">Loading...</span>
-    <img src="/images/spork-cyan.png" /></div>
+      <h5 className="cyan-text">Loading...</h5>
+      <img src="/images/spork-cyan.png" />
+    </div>
   )
 }
 
