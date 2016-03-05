0dcc87098e59311f2d20916872461b247fc477f1
Gerrit <gerrityntema@gmail.com>
2016-03-04 21:18:04 -0800

    Factor out SporkBar component

--- client/components/UserInfo.js
+++ client/components/UserInfo.js
@@ -1,5 +1,6 @@
 const React = require('react');
 const UserRepoList = require('./UserRepoList');
+const SporkBar = require('./SporkBar');
 
 class UserInfo extends React.Component {
 
@@ -27,27 +28,7 @@ class UserInfo extends React.Component {
             <h6 className="grey-text text-darken-1">{this.props.username || 'Username'}</h6>
           </div>
         </div>
-        <div className="row" style={{border: '1px solid grey'}}>
-          <div className="col s12 center-align blue-grey darken-3 z-depth-1" style={{'margin-bottom': '10px', padding: '5px'}}>
-            <span className="white-text col s4 center-align">Forks: {this.props.forks}</span>
-            <span className="white-text text-darken-1 col s4 center-align">Pulls: {this.props.pulls}</span>
-            <span className="white-text text-darken-1 col s4 center-align">Merges: {this.props.merges}</span>
-          </div>
-          <div className="col s12 left-align" style={{'margin-bottom': '10px'}}>
-            <span className="col s1 center-align mega-octicon octicon-git-branch blue-grey-text"></span>
-            <span className="col s11 fork-sporks">{this.showSporks(this.props.forks, '#965A38')}</span>
-
-          </div>
-          <div className="col s12 left-align" style={{'margin-bottom': '10px'}}>
-            <span className="col s1 center-align mega-octicon octicon-git-pull-request blue-grey-text"></span>
-            <span className="col s11 pull-sporks">{this.showSporks(this.props.pulls, '#A8A8A8')}</span>
-
-          </div>
-          <div className="col s12 left-align" style={{'margin-bottom': '10px'}}>
-            <span className="col s1 center-align mega-octicon octicon-issue-closed blue-grey-text"></span>
-            <span className="col s11 merge-sporks">{this.showSporks(this.props.merges, '#C98910')}</span>            
-          </div>
-        </div>
+        <SporkBar userInfo={this.props}/>
         <div className="col s12">        
          <UserRepoList repos={this.props.userRepos} username={this.props.username} refreshUserInfo={this.props.refreshUserInfo}/>
         </div>
