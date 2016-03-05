773273f7458fc31a2adaef2db3b518fc114170ec
Silverhawk <sondra.silverhawk@hush.com>
2016-03-04 16:25:31 -0800

    Begin adding instructions component

--- client/components/ConfirmDelete.js
+++ client/components/ConfirmDelete.js
@@ -2,42 +2,57 @@ import React, {PropTypes} from 'react';
 import {ModalContainer, ModalDialog} from 'react-modal-dialog';
 const forkUtil = require('../js/fork');
 
-class ConfirmFork extends React.Component {
+const DeleteInstructions = (props) => (
+  <div>
+    <h5>To delete the fork on GitHub:</h5>
+    <ol>
+      <li>In your repos page on GitHub, click on the link for {props.data.name}</li>
+    </lo>
+    <pre><code>git clone https://github.com/{props.forkInfo.username}/{props.data.name}.git</code></pre>
+    <p>The link depends on the repo, and can be found on GitHub.</p>
+    <p>Add the original repo as an upstream remote</p>
+    <p>In the repo's main directory, run:</p>
+    <pre><code>git remote add upstream https://github.com/{props.forkInfo.org_name}/{props.data.name}.git</code></pre>
+    <p><strong>Make changes and commit!</strong></p>
+
+    <a className="cyan-text lighten-2 cyan-text lighten-2" href="/resources">Visit our Getting Started page for more details</a>
+  </div>
+);
+
+class ConfirmDelete extends React.Component {
 
   constructor (props) {
     super(props);
     this.state = {
-      isForked: false,
-      forkInfo: {}
+      deleted: false
     }
     this.style = { borderRadius: 0 }
   }
 
-  forkRepo () {
-    forkUtil.forkRepo(function (data) {
-      console.log('successfully forked repo: ' + JSON.stringify(data));
-      this.setState({isForked: true, forkInfo: data});
+  deleteFork () {
+    forkUtil.deleteFork(function () {
+      console.log('successfully deleted repo!');
+      this.setState({deleted: true});
     }.bind(this), console.error, this.props.data.org_name, this.props.data.name, this.props.username);
   }
 
-
-
   handleClick () { this.props.openModel() }
   handleClose () { this.props.closeModal() }
-  handleFork(e) {
+  handleDelete(e) {
     e.preventDefault();
-    this.forkRepo();
+    this.deleteFork();
   }
+
   render() {
     return (<div onClick={this.handleClick}>
       {
         this.props.isShowing &&
         <ModalContainer onClose={this.handleClose.bind(this)}>
           <ModalDialog style={this.style} onClose={this.handleClose.bind(this)}>
-            <h4>You're about to fork a repo!</h4>
-            <div>this will make a fork on your GitHub account for you to start hacking on</div>
-            <a className={"btn cyan" + (this.state.isForked ? " disabled" : "")} onClick={this.handleFork.bind(this)}><i className="octicon octicon-git-forked"></i>{this.state.isForked ? "Forked!" : "Fork It!"}</a>
-            {this.state.isForked && <ForkInstructions data={this.props.data} forkInfo={this.state.forkInfo} />}
+            <h4>Are you sure you want to delete this repo?</h4>
+            <div>If you delete the repo here, it will still be forked on GitHub.</div>
+            <a className={"btn cyan" + (this.state.deleted ? " disabled" : "")} onClick={this.handleFork.bind(this)}><i className="octicon octicon-git-forked"></i>{this.state.deleted ? "Repo Deleted" : "Delete It!"}</a>
+            {this.state.deleted && <DeleteInstructions data={this.props.data} />}
           </ModalDialog>
         </ModalContainer>
       }
